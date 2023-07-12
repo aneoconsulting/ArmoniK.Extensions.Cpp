7e16264e944651642ea395da8432e473312c08ec
Dylan Brasseur <dbrasseur@aneo.fr>
2023-07-12 19:12:03 +0200

    Add properties

---
+++ ArmoniK.SDK.Common/include/Properties.h
@@ -0,0 +1,13 @@
+#ifndef ARMONIK_SDK_PROPERTIES_H
+#define ARMONIK_SDK_PROPERTIES_H
+
+#include <armonik/common/utils/IConfiguration.h>
+
+
+struct Properties {
+    armonik::api::common::utils::IConfiguration configuration;
+
+};
+
+
+#endif //ARMONIK_SDK_PROPERTIES_H
