4e5cc9166b39a0c99922732f3a2c0c8ac882b39d
Dylan Brasseur <dbrasseur@aneo.fr>
2023-08-02 16:07:42 +0200

    Unique ptr when relevant

--- ArmoniK.SDK.Client/src/SessionService.cpp
+++ ArmoniK.SDK.Client/src/SessionService.cpp
@@ -3,25 +3,40 @@
 #include <string>
 
 namespace SDK_CLIENT_NAMESPACE {
+
 SessionService::SessionService(const ArmoniK::Sdk::Common::Properties &properties)
     : impl(new Internal::SessionServiceImpl(properties)) {}
 
-std::string_view SessionService::getSession() const { return impl->getSession(); }
+const std::string &SessionService::getSession() const {
+  ensure_valid();
+  return impl->getSession();
+}
 
 std::vector<std::string>
 ArmoniK::Sdk::Client::SessionService::Submit(const std::vector<Common::TaskPayload> &requests,
                                              const std::shared_ptr<IServiceInvocationHandler> &handler,
                                              const ArmoniK::Sdk::Common::TaskOptions &task_options) {
+  ensure_valid();
   return impl->Submit(requests, handler, task_options);
 }
 std::vector<std::string>
 
 SessionService::Submit(const std::vector<Common::TaskPayload> &requests,
                        const std::shared_ptr<IServiceInvocationHandler> &handler) {
+  ensure_valid();
   return impl->Submit(requests, handler);
 }
 void SessionService::WaitResults(std::set<std::string> task_ids, WaitBehavior waitBehavior,
                                  const WaitOptions &options) {
+  ensure_valid();
   impl->WaitResults(std::move(task_ids), waitBehavior, options);
 }
+SessionService &SessionService::operator=(SessionService &&other) noexcept = default;
+void SessionService::ensure_valid() const {
+  if (!impl) {
+    throw std::runtime_error("Use after move");
+  }
+}
+SessionService::SessionService(SessionService &&) noexcept = default;
+SessionService::~SessionService() = default;
 } // namespace SDK_CLIENT_NAMESPACE
--- ArmoniK.SDK.Common/src/Configuration.cpp
+++ ArmoniK.SDK.Common/src/Configuration.cpp
@@ -12,39 +12,104 @@
 namespace SDK_COMMON_NAMESPACE {
 
 ComputePlane::ComputePlane(const Configuration &configuration)
-    : impl(std::make_shared<ArmoniK::Api::Common::options::ComputePlane>(*configuration.impl)) {}
+    : impl(std::make_unique<ArmoniK::Api::Common::options::ComputePlane>(*configuration.impl)) {}
 std::string_view ComputePlane::get_server_address() const { return impl->get_server_address(); }
 void ComputePlane::set_worker_address(std::string socket_address) {
   impl->set_worker_address(std::move(socket_address));
 }
 void ComputePlane::set_agent_address(std::string agent_address) { impl->set_agent_address(std::move(agent_address)); }
 std::string_view ComputePlane::get_agent_address() const { return impl->get_agent_address(); }
+ComputePlane::ComputePlane(const ComputePlane &computeplane)
+    : impl(std::make_unique<ArmoniK::Api::Common::options::ComputePlane>(*computeplane.impl)) {}
+ComputePlane::ComputePlane(ComputePlane &&) noexcept = default;
+ComputePlane &ComputePlane::operator=(const ComputePlane &computeplane) {
+  impl = std::make_unique<ArmoniK::Api::Common::options::ComputePlane>(*computeplane.impl);
+  return *this;
+}
+ComputePlane &ComputePlane::operator=(ComputePlane &&) noexcept = default;
+const ArmoniK::Api::Common::options::ComputePlane &ComputePlane::get_impl() const {
+  const static ArmoniK::Api::Common::options::ComputePlane default_config =
+      ArmoniK::Api::Common::options::ComputePlane(ArmoniK::Api::Common::utils::Configuration());
+  if (impl) {
+    return *impl;
+  }
+  return default_config;
+}
+ArmoniK::Api::Common::options::ComputePlane &ComputePlane::set_impl() {
+  if (!impl) {
+    impl = std::make_unique<ArmoniK::Api::Common::options::ComputePlane>(ArmoniK::Api::Common::utils::Configuration());
+  }
+  return *impl;
+}
 
 ControlPlane::ControlPlane(const Configuration &config)
-    : impl(std::make_shared<ArmoniK::Api::Common::options::ControlPlane>(*config.impl)) {}
+    : impl(std::make_unique<ArmoniK::Api::Common::options::ControlPlane>(*config.impl)) {}
 std::string_view ControlPlane::getEndpoint() const { return impl->getEndpoint(); }
 std::string_view ControlPlane::getUserCertPemPath() const { return impl->getUserCertPemPath(); }
 std::string_view ControlPlane::getUserKeyPemPath() const { return impl->getUserKeyPemPath(); }
 std::string_view ControlPlane::getUserP12Path() const { return impl->getUserP12Path(); }
 std::string_view ControlPlane::getCaCertPemPath() const { return impl->getCaCertPemPath(); }
 bool ControlPlane::isSslValidation() const { return impl->isSslValidation(); }
+ControlPlane::ControlPlane(const ControlPlane &controlplane)
+    : impl(std::make_unique<ArmoniK::Api::Common::options::ControlPlane>(*controlplane.impl)) {}
+ControlPlane::ControlPlane(ControlPlane &&) noexcept = default;
+ControlPlane &ControlPlane::operator=(const ControlPlane &controlplane) {
+  impl = std::make_unique<ArmoniK::Api::Common::options::ControlPlane>(*controlplane.impl);
+  return *this;
+}
+ControlPlane &ControlPlane::operator=(ControlPlane &&) noexcept = default;
+const ArmoniK::Api::Common::options::ControlPlane &ControlPlane::get_impl() const {
+  const static ArmoniK::Api::Common::options::ControlPlane default_config =
+      ArmoniK::Api::Common::options::ControlPlane(ArmoniK::Api::Common::utils::Configuration());
+  if (impl) {
+    return *impl;
+  }
+  return default_config;
+}
 
-Configuration::Configuration() : impl(std::make_shared<ArmoniK::Api::Common::utils::Configuration>()) {}
-Configuration::~Configuration() { impl.reset(); }
-std::string Configuration::get(const std::string &string) const { return impl->get(string); }
-void Configuration::set(const std::string &string, const std::string &value) { impl->set(string, value); }
-void Configuration::set(const Configuration &other) { impl->set(*other.impl); }
-const std::map<std::string, std::string> &Configuration::list() const { return impl->list(); }
+ArmoniK::Api::Common::options::ControlPlane &ControlPlane::set_impl() {
+  if (!impl) {
+    impl = std::make_unique<ArmoniK::Api::Common::options::ControlPlane>(ArmoniK::Api::Common::utils::Configuration());
+  }
+  return *impl;
+}
+
+Configuration::Configuration() : impl(std::make_unique<ArmoniK::Api::Common::utils::Configuration>()) {}
+Configuration::~Configuration() = default;
+std::string Configuration::get(const std::string &key) const { return get_impl().get(key); }
+void Configuration::set(const std::string &key, const std::string &value) { set_impl().set(key, value); }
+const std::map<std::string, std::string> &Configuration::list() const { return get_impl().list(); }
 Configuration &Configuration::add_json_configuration(std::string_view file_path) {
-  impl->add_json_configuration(file_path);
+  set_impl().add_json_configuration(file_path);
   return *this;
 }
 Configuration &Configuration::add_env_configuration() {
-  impl->add_env_configuration();
+  set_impl().add_env_configuration();
   return *this;
 }
 ComputePlane Configuration::get_compute_plane() const { return *this; }
 ControlPlane Configuration::get_control_plane() const { return *this; }
-Configuration::operator ArmoniK::Api::Common::utils::Configuration() { return *impl; }
+Configuration::operator ArmoniK::Api::Common::utils::Configuration() { return get_impl(); }
+Configuration::Configuration(const Configuration &config)
+    : impl(std::make_unique<ArmoniK::Api::Common::utils::Configuration>(*config.impl)) {}
+Configuration &Configuration::operator=(const Configuration &config) {
+  impl = std::make_unique<ArmoniK::Api::Common::utils::Configuration>(*config.impl);
+  return *this;
+}
+ArmoniK::Api::Common::utils::Configuration &Configuration::set_impl() {
+  if (!impl) {
+    impl = std::make_unique<ArmoniK::Api::Common::utils::Configuration>();
+  }
+  return *impl;
+}
+const ArmoniK::Api::Common::utils::Configuration &Configuration::get_impl() const {
+  const static ArmoniK::Api::Common::utils::Configuration default_config;
+  if (impl) {
+    return *impl;
+  }
+  return default_config;
+}
+Configuration::Configuration(Configuration &&) noexcept = default;
+Configuration &Configuration::operator=(Configuration &&) noexcept = default;
 
 } // namespace SDK_COMMON_NAMESPACE
