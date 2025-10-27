e0c998fecefb0034bf0a11903cca4ad9da0efb9d
José Fonseca <jfonseca@aneo.fr>
2025-10-27 11:55:03 +0100

    tweak scripts for readability

--- tools/build-client.sh
+++ tools/build-client.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
-
 # Get the absolute path of the current script and its directory
 script_path="$(dirname "${BASH_SOURCE:-$0}")"
 working_dir="$(realpath "$script_path/../" )"
@@ -11,6 +9,8 @@ IMAGE_TAG="${1:-armonik_sdk_client_test}"
 IMAGE_VERSION="${2:-"${ARMONIK_SDK_VERSION_DEFAULT}"}"
 API_VERSION="${3:-"${ARMONIK_API_VERSION_DEFAULT}"}"
 
-
-
-docker build -t "${IMAGE_TAG}:${IMAGE_VERSION}" -f ${working_dir}/ArmoniK.SDK.Client/Dockerfile --build-arg="API_VERSION=$API_VERSION" --build-arg="CLIENT_VERSION=$IMAGE_VERSION" --progress plain "${working_dir}"
+docker build -t "${IMAGE_TAG}:${IMAGE_VERSION}" \
+  -f ${working_dir}/ArmoniK.SDK.Client/Dockerfile \
+  --build-arg="API_VERSION=$API_VERSION" \
+  --build-arg="CLIENT_VERSION=$IMAGE_VERSION" \
+  --progress plain "${working_dir}"
--- tools/setup-remote.sh
+++ tools/setup-remote.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-set -x
-
 IMAGE_TAG="${1:-armonik_sdk_build_visual_studio:0.1.0}"
 
 # Get the absolute path of the current script and its directory
 script_path="$(dirname "${BASH_SOURCE:-$0}")"
 working_dir="$(realpath "$script_path/../" )"
 
-# ArmoniK.Api directory should be provided via the ARMONIK_API_DIR env var
+AK_API_DEFAULT="$(realpath "${working_dir}/../ArmoniK.Api/packages/cpp/install")"
+
 if [[ -z "${ARMONIK_API_DIR}" ]]; then
-    echo "Error: ARMONIK_API_DIR is not set."
-    exit 1
+    echo "Info: ARMONIK_API_DIR is not set"
+    echo "      Using default $AK_API_DEFAULT"
+    ARMONIK_API_DIR=AK_API_DEFAULT
 fi
 
 # Create an install directory and store its absolute path
@@ -23,4 +23,9 @@ docker build -t "${IMAGE_TAG}" -f ${script_path=}/Dockerfile.vs "${working_dir}"
 mkdir -p ${working_dir}/build
 
 # Compile the project source using the Docker image
-docker run -p 2223:22 -v "${working_dir}:/app/source" -v "${ARMONIK_API_DIR}:/armonik/api" -v "${install_dir}:/app/install" -v "${working_dir}/build:/app/build" --rm -it "${IMAGE_TAG}"
+docker run -p 2223:22 \
+    -v "${working_dir}:/app/source" \
+    -v "${ARMONIK_API_DIR}:/armonik/api" \
+    -v "${install_dir}:/app/install" \
+    -v "${working_dir}/build:/app/build" \
+    --rm -it "${IMAGE_TAG}"
